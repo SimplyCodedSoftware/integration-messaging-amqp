69df857b5526a678aa3c295ebc20c67b6e112d9c
Dariusz Gafka <dgafka.mail@gmail.com>
2018-02-26 16:30:25 +0100

    project init

---
+++ src/AmqpMessageChannel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SimplyCodedSoftware\IntegrationMessaging\Amqp;
+
+/**
+ * Class AmqpMessageChannel
+ * @package SimplyCodedSoftware\IntegrationMessaging\Amqp
+ * @author  Dariusz Gafka <dgafka.mail@gmail.com>
+ */
+class AmqpMessageChannel
+{
+
+}
